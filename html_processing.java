a2a7d45d16a68953c659ba4b5341fe5e43312ad8
Jaswanth Reddy <143704905+JaswanthReddy2006@users.noreply.github.com>
2025-08-21 22:55:28 +0530

    v1.1
     added html_processing (just an test class ) compled usage will be added in next update.

--- html_processing.java
+++ html_processing.java
@@ -1,6 +1,28 @@
-package org.example;
-//import org.jsoup.Jsoup;
+import java.io.File;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
-class html_processing {
-    void
+class HTML_processing {
+    void html_process(String path) {
+        try {
+            File file = new File(path);
+            Document doc = Jsoup.parse(file, "UTF-8");
+
+            for(Element link : doc.select("a")) {
+                System.out.println(link.attr("href"));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+class test{
+    public static void main(String[]agg) {
+        HTML_processing obj = new HTML_processing();
+        obj.html_process("D:\\gdrive upload\\project_dump\\www.geeksforgeeks.org_.html");
+    }
+
 }
